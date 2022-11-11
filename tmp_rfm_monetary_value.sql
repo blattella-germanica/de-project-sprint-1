eda4423dcb79b926eaa6d587f75e7a4ccb0da1c5
Elena Belova <nuibarash@yandex.ru>
2022-11-11 23:39:42 +0300

    third iteration

--- tmp_rfm_monetary_value.sql
+++ tmp_rfm_monetary_value.sql
@@ -1,7 +1,7 @@
 INSERT INTO analysis.tmp_rfm_monetary_value (user_id, monetary_value)
 (
 SELECT id AS user_id,
-	   NTILE(5) OVER (ORDER BY SUM(cost)) AS monetary_value
+	   NTILE(5) OVER (ORDER BY SUM(coalesce(cost, 0))) AS monetary_value
 FROM analysis.users_view uv
 LEFT JOIN (SELECT cost,
 		   		  user_id
--- tmp_rfm_recency.sql
+++ tmp_rfm_recency.sql
@@ -1,7 +1,7 @@
 INSERT INTO analysis.tmp_rfm_recency (user_id, recency)
 (
 SELECT id as user_id,
-		NTILE(5) OVER (ORDER BY MAX(ov.order_ts)) AS recency
+		NTILE(5) OVER (ORDER BY MAX(coalesce(ov.order_ts, '1900-01-01 00:00:00.000'))) AS recency
 FROM analysis.users_view uv
 LEFT JOIN (SELECT order_ts,
 		   			user_id
